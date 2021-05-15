3634354d490397ce2b3e1123e1a48c173a0b196f
Qiaoliwang2020 <414217795zoe@gmail.com>
2021-05-15 17:15:58 +1000

    set user id to the attribute of dom

--- public/env.js
+++ public/env.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   // get user name and user icon from App Id
   $.getJSON('/home/api/idPayload', function (id_token) {
     $('#userNameSpan').html(id_token.name);
+    $('#userNameSpan').attr('data-userid',id_token.sub);
     $('#user-icon').attr('src',id_token.picture);
   });
   // initialize modal
